35a867705cf45b46fa34410b061d32c39bd44115
Amber Jain <jainanuamber@gmail.com>
2024-04-09 14:39:53 +0530

    added dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("com.mesibo.api:webrtc:1.0.5")
+    implementation("org.java-websocket:Java-WebSocket:1.5.3")
+
 }
