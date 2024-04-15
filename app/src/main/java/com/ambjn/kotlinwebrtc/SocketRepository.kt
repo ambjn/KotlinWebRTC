561a565af8a596952974fb55ff6516e0c0efca87
Amber Jain <jainanuamber@gmail.com>
2024-04-15 15:52:48 +0530

    updated

--- app/src/main/java/com/ambjn/kotlinwebrtc/SocketRepository.kt
+++ app/src/main/java/com/ambjn/kotlinwebrtc/SocketRepository.kt
@@ -2,7 +2,8 @@ package com.ambjn.kotlinwebrtc
 
 import org.java_websocket.WebSocket
 
-class SocketRepository {  private var webSocket: WebSocket? = null;
+class SocketRepository {
+    private var webSocket: WebSocket? = null;
     private var userName: String? = null
     private val TAG = "SocketRepository"
 }
